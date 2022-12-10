38374e34cb126850f233ddb45e93d6d00b6439fd
Andrew Garrett <andrewgarrett@rivian.com>
2022-12-09 20:12:05 -0800

    device connects on container start, testing display

--- main.py
+++ main.py
@@ -22,14 +22,14 @@ from displayPipeline import DisplayPipeline
 if __name__ == "__main__":
     oak_cam = OAKPipeline()
     oak_processor = ProcessingPipeline()
-    # oak_display = DisplayPipeline()
+    oak_display = DisplayPipeline()
     oak_cam.startDevice()
     counter = 1
     t0 = time()
     while oak_cam.isOpened():
         oak_cam.read()
         oak_processor.processPayload(oak_cam.frame_dict)
-        # oak_display.show(oak_cam.frame_dict)
+        oak_display.show(oak_cam.frame_dict)
         if counter % 100 == 0:
             dt = time() - t0
             print("Time Elapsed: ", time() - t0)
