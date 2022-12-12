63062706d50d836655c9f3313c4ba699722fdf58
andrew-garrett <118957273+andrew-garrett@users.noreply.github.com>
2022-12-12 14:23:21 -0800

    more small tweaks to logging

--- oak_capture/setup_logging.py
+++ oak_capture/setup_logging.py
@@ -3,17 +3,15 @@ import logging
 class CustomFormatter(logging.Formatter):
 	"""Logging colored formatter, adapted from https://stackoverflow.com/a/56944256/3638629"""
 
-	grey = '\x1b[38;21m'
-	blue = '\x1b[38;5;39m'
-	yellow = '\x1b[38;5;226m'
-	red = '\x1b[38;5;196m'
-	bold_red = '\x1b[31;1m'
-	reset = '\x1b[0m'
-	formatting = "%(asctime)s - %(name)s - %(levelname)s - %(message)s - %(threadName)s - (%(filename)s:%(lineno)d)"
-
 	def __init__(self):
 		super().__init__()
-		self.fmt = formatting
+		self.grey = '\x1b[38;21m'
+		self.blue = '\x1b[38;5;39m'
+		self.yellow = '\x1b[38;5;226m'
+		self.red = '\x1b[38;5;196m'
+		self.bold_red = '\x1b[31;1m'
+		self.reset = '\x1b[0m'
+		self.fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s - %(threadName)s - (%(filename)s:%(lineno)d)"
 		self.FORMATS = {
 			logging.DEBUG: self.grey + self.fmt + self.reset,
 			logging.INFO: self.blue + self.fmt + self.reset,
