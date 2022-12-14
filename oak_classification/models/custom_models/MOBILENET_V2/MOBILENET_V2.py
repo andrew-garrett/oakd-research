d345f8452e44713ff19db0dd1adea7fa68624130
andrew-garrett <118957273+andrew-garrett@users.noreply.github.com>
2022-12-13 19:50:04 -0800

    wrapper class?

--- oak_classification/models/custom_models/MOBILENET_V2/MOBILENET_V2.py
+++ oak_classification/models/custom_models/MOBILENET_V2/MOBILENET_V2.py
@@ -1,13 +1,15 @@
 import torch.nn as nn
 import torchvision.models
 
-def MOBILENET_V2(pretrained=True, num_classes=10, feature_extracting=True):
-	# Create a model instance of the MobileNet V2 Architecture, pretrained on ImageNet
-	model = torchvision.models.mobilenet_v2(pretrained=pretrained)
-	# If specified, freeze the feature extractor
-	if feature_extracting:
-		for param in model.parameters():
-			param.requires_grad = False
-	# Replace the classifier (which outputs 1000 classes for ImageNet) with a classifier which outputs 10 classes for CIFAR-10
-	model.classifier[1] = nn.Linear(in_features=model.last_channel, out_features=num_classes)
-	return model
+class MOBILENET_V2:
+	def __init__(self, pretrained=True, num_classes=10, feature_extracting=True):
+		# def MOBILENET_V2(pretrained=True, num_classes=10, feature_extracting=True):
+		# Create a model instance of the MobileNet V2 Architecture, pretrained on ImageNet
+		model = torchvision.models.mobilenet_v2(pretrained=pretrained)
+		# If specified, freeze the feature extractor
+		if feature_extracting:
+			for param in model.parameters():
+				param.requires_grad = False
+		# Replace the classifier (which outputs 1000 classes for ImageNet) with a classifier which outputs 10 classes for CIFAR-10
+		model.classifier[1] = nn.Linear(in_features=model.last_channel, out_features=num_classes)
+		return model
