2052ca05299a8d884563baf142778876da632b44
andrew-garrett <118957273+andrew-garrett@users.noreply.github.com>
2022-12-14 00:12:22 -0800

    tweak

--- oak_classification/models/custom_models/MOBILENET_V2/MOBILENET_V2.py
+++ oak_classification/models/custom_models/MOBILENET_V2/MOBILENET_V2.py
@@ -20,20 +20,20 @@ class MOBILENET_V2(nn.Module):
 			weights = "DEFAULT"
 		else:
 			weights = None
-		self.model = torchvision.models.mobilenet_v2(weights=weights)
+		self.m = torchvision.models.mobilenet_v2(weights=weights)
 		# If specified, freeze the feature extractor
 		if feature_extracting:
-			for param in self.model.parameters():
+			for param in self.m.parameters():
 				param.requires_grad = False
 		# Replace the classifier (which outputs 1000 classes for ImageNet) with a classifier which outputs 10 classes for CIFAR-10
 		custom_classifier = nn.Sequential(
-			nn.Linear(in_features=self.model.last_channel, out_features=4*num_classes),
+			nn.Linear(in_features=self.m.last_channel, out_features=4*num_classes),
 			nn.Linear(in_features=4*num_classes, out_features=num_classes)
 		)
-		self.model.classifier[1] = custom_classifier
+		self.m.classifier[1] = custom_classifier
 
 	def forward(self, x):
-		return self.model(x)
+		return self.m(x)
 
 
 
